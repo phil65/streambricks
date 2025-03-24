c33c4d26eac3b2ccd2dae3e6160073180a8ca7ff
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-24 07:09:45 +0100

    chore: adjust exported names

--- src/streambricks/__init__.py
+++ src/streambricks/__init__.py
@@ -1,16 +1,23 @@
 __version__ = "0.2.1"
 
 
-from streambricks.widgets.model_widget import render_model_form, render_model_readonly
+from streambricks.widgets.model_widget import (
+    render_model_form as model_edit,
+    render_model_readonly as model_display,
+)
 from streambricks.widgets.multi_select import multiselect, MultiSelectItem
-from streambricks.widgets.model_selector import model_selector
+from streambricks.widgets.model_selector import (
+    model_selector,
+    model_selector as llm_model_selector,
+)
 from streambricks.helpers import run
 
 __all__ = [
     "MultiSelectItem",
+    "llm_model_selector",
+    "model_display",
+    "model_edit",
     "model_selector",
     "multiselect",
-    "render_model_form",
-    "render_model_readonly",
     "run",
 ]
