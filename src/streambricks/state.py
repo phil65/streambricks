30c3b6d0b8ab8d17c5e107bd682047ae0809cac5
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-03-24 10:25:58 +0100

    chore: set / reset for State

--- src/streambricks/state.py
+++ src/streambricks/state.py
@@ -30,3 +30,24 @@ class State(BaseModel):
             st.session_state[state_key] = cls()
 
         return st.session_state[state_key]
+
+    @classmethod
+    def set(cls, *, key: str | None = None, value: Self):
+        """Set state to given model.
+
+        Args:
+            key: Optional key to use in session_state (defaults to model class name)
+            value: The value to set in session_state
+        """
+        state_key = key or cls.__name__
+        st.session_state[state_key] = value
+
+    @classmethod
+    def reset(cls, *, key: str | None = None):
+        """Reset state.
+
+        Args:
+            key: Optional key to use in session_state (defaults to model class name)
+        """
+        state_key = key or cls.__name__
+        st.session_state[state_key] = cls()
