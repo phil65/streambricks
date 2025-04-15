c41ec0ef35027a0cfce3fde628fda8f80e2227ce
Philipp Temminghoff <philipptemminghoff@googlemail.com>
2025-04-15 03:09:49 +0200

    chore: add image_capture

--- src/streambricks/__init__.py
+++ src/streambricks/__init__.py
@@ -6,6 +6,7 @@ from streambricks.widgets.model_widget import (
     render_model_readonly as model_display,
 )
 from streambricks.widgets.multi_select import multiselect, MultiSelectItem
+from streambricks.widgets.image_capture import image_capture
 from streambricks.widgets.model_selector import (
     model_selector,
     model_selector as llm_model_selector,
@@ -18,6 +19,7 @@ __all__ = [
     "MultiSelectItem",
     "State",
     "bind_kwargs_as_widget",
+    "image_capture",
     "llm_model_selector",
     "model_display",
     "model_edit",
